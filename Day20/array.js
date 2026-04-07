691b414a6a15791cf8dfa6ccc4ea105ed0d5445c
Saraswathir2003 <saraswathir003@gmail.com>
2026-04-07 21:22:48 +0530

    Add files via upload

---
+++ Day20/array.js
@@ -0,0 +1,19 @@
+let array = [10, 20, 30];
+
+console.log("Original Array:", array);
+
+// push → adds element at the end
+array.push(40);
+console.log("After push:", array);
+
+// pop → removes last element
+array.pop();
+console.log("After pop:", array);
+
+// unshift → adds element at the beginning
+array.unshift(5);
+console.log("After unshift:", array);
+
+// shift → removes first element
+array.shift();
+console.log("After shift:", array);
